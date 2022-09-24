33daf79fade34099ecdae2186e890bbfd701d96a
ercumentlacin <ercument.lacin@akinon.com>
2022-09-24 14:08:08 +0300

    add timestamp & add pre function for password hash

--- src/modules/auth/Auth.ts
+++ src/modules/auth/Auth.ts
@@ -1,11 +1,24 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
 import type { AuthDocument, AuthModel } from './interface';
 
-const authSchema = new Schema<AuthDocument, AuthModel>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  avatar: String,
+const authSchema = new Schema<AuthDocument, AuthModel>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatar: String,
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+authSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
 });
 
 const Auth = model<AuthDocument, AuthModel>('Auth', authSchema);
